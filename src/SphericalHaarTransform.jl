59a668ac18b3c3bbbc8a798e64a77fedac999354
Aria Radick <aradick@uoregon.edu>
2025-04-02 13:29:12 -0700

    export evaluate_f

--- src/SphericalHaarTransform.jl
+++ src/SphericalHaarTransform.jl
@@ -1,6 +1,7 @@
 module SphericalHaarTransform
 
-export sph_haar_transform, sph_haar_inverse, sph_haar_points, sph_haar_index
+export sph_haar_transform, sph_haar_inverse, sph_haar_points, sph_haar_index,
+    evaluate_f
 
 using QuadGK
 using FastSphericalHarmonics
